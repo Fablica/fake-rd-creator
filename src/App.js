bc0468fcdff2b2f81d82f17c6abc047e486f5862
Kento75 <kento2github@gmail.com>
2018-10-05 22:25:03 +0900

    Add base app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import TopPage from "./components/TopPage/TopPage";
+import HomePage from "./components/HomePage/HomePage";
+import NotFoundPage from "./components/NotFoundPage/NotFoundPage";
 
-export default App;
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">TopPage</Link></li>
+        <li><Link to="/home">HomePage</Link></li>
+      </ul>
+      <Switch>
+        <Route exact path="/" component={TopPage} />
+        <Route path="/home" component={HomePage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </Router>
+)
+export default App
---
+++ src/components/TopPage/TopPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TopPage = () => {
+  return (
+    <div>
+      <h1>TopPage</h1>
+    </div>
+  );
+};
+
+export default TopPage;
