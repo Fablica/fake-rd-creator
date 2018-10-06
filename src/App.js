1dcbb67595a32d97279a7cb09746e03dbd006f17
Kento75 <kento2github@gmail.com>
2018-10-07 05:17:01 +0900

    Add history logic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import { history } from "./helpers";
 import PageHeader from "./components/PageHeader/PageHeader";
 import TopPage from "./components/TopPage/TopPage";
 import HomePage from "./components/HomePage/HomePage";
 import NotFoundPage from "./components/NotFoundPage/NotFoundPage";
 
-const App = () => (
-  <Router>
-    <div>
-      <PageHeader />
-      <Switch>
-        <Route exact path="/" component={TopPage} />
-        <Route path="/home" component={HomePage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </div>
-  </Router>
-);
+class App extends Component {
+  render() {
+    return (
+      <Router history={history}>
+        <div>
+          <PageHeader />
+          <Switch>
+            <Route exact path="/" component={TopPage} />
+            <Route path="/home" component={HomePage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
 export default App;
