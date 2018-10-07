13bd4aa9b4692405a2f88e4cf3c5303e4fc32abe
Kento75 <kento2github@gmail.com>
2018-10-08 08:57:52 +0900

    Updates

---
+++ precache-manifest.9c95a289c7929886132f4c93d41c9283.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/fake-rd-creator/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "d07287320e52f30e3da3",
+    "url": "/fake-rd-creator/static/css/main.a35a7da4.chunk.css"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/fake-rd-creator/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "65af0b2b31bea75e2e3f",
+    "url": "/fake-rd-creator/static/js/1.65af0b2b.chunk.js"
+  },
+  {
+    "revision": "9767e3d33514de851585",
+    "url": "/fake-rd-creator/static/js/runtime~main.9767e3d3.js"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/fake-rd-creator/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/fake-rd-creator/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/fake-rd-creator/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/fake-rd-creator/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/fake-rd-creator/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/fake-rd-creator/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "d07287320e52f30e3da3",
+    "url": "/fake-rd-creator/static/js/main.d0728732.chunk.js"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/fake-rd-creator/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/fake-rd-creator/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/fake-rd-creator/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/fake-rd-creator/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/fake-rd-creator/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/fake-rd-creator/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/fake-rd-creator/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/fake-rd-creator/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "65af0b2b31bea75e2e3f",
+    "url": "/fake-rd-creator/static/css/1.67b1bc08.chunk.css"
+  },
+  {
+    "revision": "cf1e925e7280f62b30a3f1d1d1b4fb1f",
+    "url": "/fake-rd-creator/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/fake-rd-creator/precache-manifest.9c95a289c7929886132f4c93d41c9283.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/fake-rd-creator/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
