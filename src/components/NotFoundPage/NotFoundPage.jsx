ea222d60fba008f562e28156175f10d578a13aaa
Kento75 <kento2github@gmail.com>
2018-10-09 05:57:03 +0900

    Fix NotFoundPage route

--- src/components/NotFoundPage/NotFoundPage.jsx
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -9,7 +9,7 @@ const NotFoundPage = () => {
           <div className="content">404 Not Found</div>
         </h1>
         <br />
-        <Link className="ui huge white inverted button" to="/">
+        <Link className="ui huge white inverted button" to="/fake-rd-creator">
           Top Page
           <i className="right arrow icon" />
         </Link>
