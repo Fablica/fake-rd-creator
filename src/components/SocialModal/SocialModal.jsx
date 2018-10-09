060f9a92e7ecbcb016286cf79ce5ec64e5230dac
Kento75 <kento2github@gmail.com>
2018-10-09 21:02:05 +0900

    Add Social Share Buttons components

--- src/components/SocialModal/SocialModal.jsx
+++ src/components/SocialModal/SocialModal.jsx
@@ -1,14 +1,49 @@
 import React from "react";
-import { Modal, Button, Icon } from "semantic-ui-react";
+import { Modal } from "semantic-ui-react";
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  LinkedinShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  LinkedinIcon
+} from "react-share";
 
 const SocialModal = ({ showSocialDialog, OnClickSocialButton }) => {
+  const shareUrl = "https://kento75.github.io/fake-rd-creator/";
+  const title = "fake-rd-creator";
   return (
-    <Modal open={showSocialDialog}>
-      <Modal.Header>Social Links</Modal.Header>
-      <Modal.Content>
-        <Button color="twitter" onClick={() => OnClickSocialButton()}>
-          <Icon name="twitter" /> Twitter
-        </Button>
+    <Modal
+      size="mini"
+      open={showSocialDialog}
+      closeIcon
+      centered={false}
+      onClick={() => OnClickSocialButton()}
+    >
+      <Modal.Header>Share Buttons</Modal.Header>
+      <Modal.Content style={{ display: "flex", justifyContent: "center" }}>
+        <FacebookShareButton
+          url={shareUrl}
+          quote={title}
+          style={{ paddingRight: "30px" }}
+        >
+          <FacebookIcon size={40} round />
+        </FacebookShareButton>
+        <TwitterShareButton
+          url={shareUrl}
+          title={title}
+        >
+          <TwitterIcon size={40} round />
+        </TwitterShareButton>
+        <LinkedinShareButton
+          url={shareUrl}
+          title={title}
+          windowWidth={750}
+          windowHeight={600}
+          style={{ paddingLeft: "30px" }}
+        >
+          <LinkedinIcon size={40} round />
+        </LinkedinShareButton>
       </Modal.Content>
     </Modal>
   );
